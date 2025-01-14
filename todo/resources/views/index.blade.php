92882fb4b7200952ec0c35a3331383d5c9b18b19
EC2 Default User <ec2-user@ip-172-31-10-11.ap-northeast-1.compute.internal>
2025-01-14 08:02:51 +0000

    ログイン機能

---
+++ todo/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>todo</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    <form action="/login" method="post">
+        name:<input type="name" name="name"><br>
+        email:<input type="text" name="email"><br>
+        pass:<input type="passord" name="password"><br>
+        <button>ログインする</button>
+    </form>
+</body>
+</html>
